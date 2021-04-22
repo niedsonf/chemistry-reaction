1d375f27f22c25c366186e30485dc9886e6f266c
Niedson Fernandes <niofernandodf@gmail.com>
2021-04-22 14:49:18 -0300

    add create sprite function in game scene

--- script.js
+++ script.js
@@ -14,9 +14,18 @@ class MenuScene extends Phaser.Scene {
     create(){
 
         this.add.image(0, 0, 'menu').setOrigin(0);
-        var jogar = this.add.sprite(50, 250, 'jogar').setOrigin(0).setInteractive();
-        var guia = this.add.sprite(400, 250, 'guia').setOrigin(0).setInteractive();
-        var creditos = this.add.sprite(750, 250, 'creditos').setOrigin(0).setInteractive();
+
+        var jogar = this.add.sprite(50, 250, 'jogar')
+            .setOrigin(0)
+            .setInteractive();
+
+        var guia = this.add.sprite(400, 250, 'guia')
+            .setOrigin(0)
+            .setInteractive();
+
+        var creditos = this.add.sprite(750, 250, 'creditos')
+            .setOrigin(0)
+            .setInteractive();
 
         this.input.on('gameobjectover', function (pointer, gameObject) {
             gameObject.alpha = 0.8;
@@ -37,7 +46,6 @@ class MenuScene extends Phaser.Scene {
         creditos.on('pointerup', function (pointer){
             this.scene.start('CreditScene');
         }, this);
-
     }
 }
 
@@ -71,10 +79,12 @@ class GameScene extends Phaser.Scene {
     }   
 
     create (){
+
         const posx = [786, 951], posy = 27;
         var popup, popupisOpen = false;
         var slot = [false, false];
-        var reactaux = {
+        var reactaux = 
+        {
             agno3: false,
             fecl3: false,
             cuso4: false,
@@ -84,7 +94,8 @@ class GameScene extends Phaser.Scene {
             cacl2: false
         };
         
-        this.add.image(0, 0, 'bg').setOrigin(0);
+        this.add.image(0, 0, 'bg')
+            .setOrigin(0);
 
         this.input.on('gameobjectover', function (pointer, gameObject) {
             gameObject.alpha = 0.8;
@@ -95,57 +106,44 @@ class GameScene extends Phaser.Scene {
             gameObject.alpha = 1;
         });
 
-        var text = this.add.text(600, 300, 'SELECIONE DOIS\n    REAGENTES', {
+        var text = this.add.text(600, 300, 'SELECIONE DOIS\n    REAGENTES', 
+        {
             fontFamily: 'Forced Square',
             fontSize: 20
         });
     
         /// --- ADD SPRITES --- ///
-        var agno3 = this.add.sprite(25, 25, 'agno3')
-            .setOrigin(0)
-            .setInteractive()
+        var agno3 = createSprite(25, 25, 'agno3', this)
             .on('pointerup', function (pointer){
                 reactaux.agno3 = selecting(agno3, 25, 25, reactaux.agno3);
         });
     
-        var fecl3 = this.add.sprite(178, 25, 'fecl3')
-            .setOrigin(0)
-            .setInteractive()
+        var fecl3 = createSprite(178, 25, 'fecl3', this)
             .on('pointerup', function (pointer){
                 reactaux.fecl3 = selecting(fecl3, 178, 25, reactaux.fecl3);
         });
     
-        var cuso4 = this.add.sprite(331, 25, 'cuso4')
-            .setOrigin(0)
-            .setInteractive()
+        var cuso4 = createSprite(331, 25, 'cuso4', this)
             .on('pointerup', function (pointer){
                 reactaux.cuso4 = selecting(cuso4, 331, 25, reactaux.cuso4);
         });
         
-        var na2co3 = this.add.sprite(25, 256, 'na2co3')
-            .setOrigin(0)
-            .setInteractive()
+        var na2co3 = createSprite(25, 256, 'na2co3', this)
             .on('pointerup', function (pointer){
                 reactaux.na2co3 = selecting(na2co3, 25, 256, reactaux.na2co3);
         });
     
-        var k2so4 = this.add.sprite(178, 256, 'k2so4')
-            .setOrigin(0)
-            .setInteractive()
+        var k2so4 = createSprite(178, 256, 'k2so4', this)
             .on('pointerup', function (pointer){
                 reactaux.k2so4 = selecting(k2so4, 178, 256, reactaux.k2so4);
         });
     
-        var naoh = this.add.sprite(331, 256, 'naoh')
-            .setOrigin(0)
-            .setInteractive()
+        var naoh = createSprite(331, 256, 'naoh', this)
             .on('pointerup', function (pointer){
                 reactaux.naoh = selecting(naoh, 331, 256, reactaux.naoh);
         });
     
-        var cacl2 = this.add.sprite(484, 256, 'cacl2')
-            .setOrigin(0)
-            .setInteractive()
+        var cacl2 = createSprite(484, 256, 'cacl2', this)
             .on('pointerup', function (pointer){
                 reactaux.cacl2 = selecting(cacl2, 484, 256, reactaux.cacl2);
         });
@@ -154,28 +152,27 @@ class GameScene extends Phaser.Scene {
         var reacoes = this.add.sprite(635, 49, 'reacoes')
             .setInteractive()
             .on('pointerup', function(pointer){
-            if(!popupisOpen){
-                popup = this.add.image(25, 30, 'list')
-                    .setInteractive()
-                    .setOrigin(0);
-                popupisOpen = true;
-            }
-            popup.on('pointerup', function (pointer){
-                popup.destroy();
-                popupisOpen = false;
-            });
-        }, this);
+                if(!popupisOpen){
+                    popup = this.add.image(25, 30, 'list')
+                        .setInteractive()
+                        .setOrigin(0);
+                    popupisOpen = true;
+                }
+                popup.on('pointerup', function (pointer){
+                    popup.destroy();
+                    popupisOpen = false;
+                });
+            }, this);
 
         /// --- PUSH BALANCE SCENE --- ///
-        var redcheck = this.add.sprite(1000, 256, 'redcheck')
-            .setOrigin(0)
-            .setInteractive()
+        var redcheck = createSprite(1000, 256, 'redcheck', this)
             .on('pointerup', function (pointer){
                 var toNext = [];
                 if(slot[0] && slot[1]){
                     for(let i in reactaux){
                         if(reactaux[i] == true){toNext.push(i)}
                     }
+                    console.log(toNext);
                     switch(true){
                     default:
                         defaultConfig();
@@ -221,6 +218,13 @@ class GameScene extends Phaser.Scene {
             }, this);
 
         /// --- FUNCTIONS --- ///
+        function createSprite(x, y, reference, scene){
+            let newSprite = scene.add.sprite(x, y, reference)
+                .setOrigin(0)
+                .setInteractive()
+            return newSprite;
+        }
+
         function defaultConfig(){
             agno3.setPosition(25, 25);
             fecl3.setPosition(178, 25);
@@ -297,60 +301,66 @@ class BalanceScene extends Phaser.Scene {
         this.load.image('r8', 'assets/r8.png');
         this.load.image('r9', 'assets/r9.png');
         this.load.image('inputbox', 'assets/inputbox.png');
-        this.load.plugin('rextexteditplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rextexteditplugin.min.js', true);
-        this.load.plugin('rexbbcodetextplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexbbcodetextplugin.min.js', true);
+        //this.load.plugin('rextexteditplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rextexteditplugin.min.js', true);
+        //this.load.plugin('rexbbcodetextplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexbbcodetextplugin.min.js', true);
+        this.load.plugin('rexinputtextplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexinputtextplugin.min.js', true);  
     }
 
     create(data){
+
         const posx = [786, 951], posy = 27;
-        var balancebg = this.add.image(0, 0, 'balancebg').setOrigin(0);
-    
-        this.add.image(posx[0], posy, data[0]).setOrigin(0);
-        this.add.image(posx[1], posy, data[1]).setOrigin(0);
+
+        var balancebg = this.add.image(0, 0, 'balancebg')
+            .setOrigin(0);
+
+        this.add.image(posx[0], posy, data[0])
+            .setOrigin(0);
+
+        this.add.image(posx[1], posy, data[1])
+            .setOrigin(0);
+
         var eq = this.add.image(550, 400, data[2]);
-        var inputText = this.add.image(50, 50, 'inputbox').setOrigin(0);
 
-        var coefV = generateInput(110, 65, this)
-            .on('pointerdown', function () {
-                this.plugins.get('rextexteditplugin').edit(coefV, config);
-            }, this); 
+        var inputText = this.add.image(50, 50, 'inputbox')
+            .setOrigin(0);
 
-        var coefW = generateInput(110, 133, this)
-            .on('pointerdown', function () {
-                this.plugins.get('rextexteditplugin').edit(coefW, config);
-            }, this);
-            
-        var coefX = generateInput(225, 65, this)
-            .on('pointerdown', function () {
-                this.plugins.get('rextexteditplugin').edit(coefX, config);
-            }, this);
+        var redcheck = this.add.sprite(300, 80, 'redcheck')
+            .setOrigin(0)
+            .setInteractive()
+            .on('pointerup', function(pointer){
+                let collectCoef = [];
+                for(let i in coef){
+                    collectCoef.push(coef[i].text)
+                }
+                console.log(collectCoef)
 
-        var coefZ = generateInput(225, 133, this)
-            .on('pointerdown', function () {
-                this.plugins.get('rextexteditplugin').edit(coefZ, config);
-            }, this);
+            });
 
-
-        var config = {
-            type: 'number',
-            onTextChanged: function (textObject, text) {
-                textObject.text = text;
-            }
-        }; 
+        var coef = 
+        {
+            v: generateInput(110, 65, this),
+            w: generateInput(225, 65, this),
+            x: generateInput(110, 133, this),
+            z: generateInput(225, 133, this)
+        }    
 
         function generateInput(x, y, scene){
-            let gInput = scene.add.rexBBCodeText(x, y, '0', {
+            let gInput = scene.add.rexInputText(x, y, 55, 35, 
+            {
+                type: 'number',
+                text: '0',
                 color: 'blueviolet',
                 fontSize: '35px',
                 fontFamily: 'Forced Square',
-                fixedWidth: 55,
-                fixedHeight: 35,
                 backgroundColor: '#333333',
                 align: 'center'
-                
             })
-            .setOrigin(0)
-            .setInteractive() 
+                .setOrigin(0)
+                gInput.node.addEventListener("keypress", function (evt) {
+                    if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57) {
+                        evt.preventDefault();
+                    }
+                });
             return gInput;
         }    
     }
